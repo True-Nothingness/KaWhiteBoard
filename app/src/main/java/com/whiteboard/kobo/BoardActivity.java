8208248a685d282f32fbb29f07d39bdae5d2e909
True-Nothingness <72776760+True-Nothingness@users.noreply.github.com>
2023-12-16 14:22:54 +0700

    First Commit

--- app/src/main/java/com/whiteboard/kobo/BoardActivity.java
+++ app/src/main/java/com/whiteboard/kobo/BoardActivity.java
@@ -8,6 +8,7 @@ public class BoardActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board);
     }
